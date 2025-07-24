05d57d4ba099afff699fc180566561b466419f62
Ali <89438079+AliBa1@users.noreply.github.com>
2025-07-24 08:27:08 -0700

    handler func

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import "net/http"
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", healthzHandler)
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 	mux.Handle("/assets", http.FileServer(http.Dir("./assets")))
 	server := http.Server{
 		Handler: mux,
